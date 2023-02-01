db0a5287d69ff41286a43456fea92060f269fd68
gunawan-ex <110682243+gunawan-ex@users.noreply.github.com>
2023-02-01 09:46:49 +0800

    Web v1.0

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// TOGGLE CLASS ACTIVE
+const navbarNav = document.querySelector('.navbar-nav');
+// KETIKA HUMBURGER MENU DIKLIK
+onclick = () => {
+  document.querySelector('#humburger-menu');
+  navbarNav.classList.toggle('active');
+}
