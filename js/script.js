5ca170b6ad315ccc066d218d3610c8b45302aba6
gunawan-ex <110682243+gunawan-ex@users.noreply.github.com>
2023-02-01 11:23:11 +0800

    3 files modified

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ onclick = () => {
   document.querySelector('#humburger-menu');
   navbarNav.classList.toggle('active');
 }
+
+// KLIK DI KUAR SB UNTUK MENGHILANGKAN NAV
+const humburger = document.querySelector('#humburger-menu');
+
+document.addEventListener('click', function(e) {
+  if (!humburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active')
+  }
+});
